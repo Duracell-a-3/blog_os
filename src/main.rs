d8b5fce0337102fe3a11e2c6752c3637b023a81f
Duracell-a-3 <duracell.a.3@gmail.com>
2021-12-30 20:40:45 -0600

    v5

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,12 @@ fn panic(_info: &PanicInfo) -> ! {
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x4;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
